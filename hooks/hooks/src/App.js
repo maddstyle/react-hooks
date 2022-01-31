a878fc60e9f3902adfeb55197c6e7691bb3a6d32
Michaela DeCamara <michaela.decamara@gmail.com>
2022-01-31 01:53:15 -0500

    useState and useEffect hook

--- hooks/hooks/src/App.js
+++ hooks/hooks/src/App.js
@@ -1,9 +1,49 @@
+import React, { Component, useState, useEffect } from "react";
 
+// class App extends Component {
+//   state = {
+//     count: 0
+//   };
 
-function App() {
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count +1
+//     });
+//   };
+
+//   componentDidMount() {
+//     document.title = `Clicked ${this.state.count} times`;
+//   }
+
+//   componentDidUpdate() {
+//     document.title = `Clicked ${this.state.count} times`;
+//   }
+  
+//   render() {
+//     return (
+//       <div className="App">
+//         <h2>Counter App</h2>
+//         <button onClick={this.increment}>Clicked {this.state.count} times</button>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Clicked ${count} times`
+  });
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <h2>Counter App</h2>
+      <button onClick={increment}>Clicked {count} times</button>
     </div>
   );
 }
